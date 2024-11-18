5f1bd2f50e1c2e2d417eb39dcec6a97288f696a7
BitCoderSB <152751173+BitCoderSB@users.noreply.github.com>
2024-11-17 21:52:53 -0600

    conexion a pagina

--- FitManage/src/index.js
+++ FitManage/src/index.js
@@ -1,1 +1,9 @@
-console.log("fjhda");
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => res.send("Hello cat"));
+
+app.listen(3000);
+
+console.log("Server ", 3000);
